6dcd4820bb3ff84ef09ccff246f5c4d1db529107
Takuya Endo <endo@Takuyas-MacBook-Pro.local>
2016-09-23 17:31:02 +0900

    天気予報アプリ作成完了

--- Whats The Wheather/Whats The Wheather/ViewController.swift
+++ Whats The Wheather/Whats The Wheather/ViewController.swift
@@ -10,46 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var cityTextField: UITextField!
-    
-    @IBOutlet var resultLabel: UILabel!
-    @IBAction func findWeather(sender: AnyObject) {
-        
-        
 
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+
+    @IBAction func findWeather(sender: AnyObject) {
+        let attemptedUrl = NSURL(string: "http://www.weather-forecast.com/locations/" + cityTextField.text!.stringByReplacingOccurrencesOfString("　", withString: "-") + "/forecasts/latest")
         
-        let url = NSURL(string: "http://www.weather-forecast.com/locations/Paris/forecasts/latest")!
+        var wasSuccessful = false
         
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+        if let url = attemptedUrl{
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
                 if let urlContent = data {
-                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                    let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
                     
-                let websiteArray = webContent?.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
-                
+                    let websiteArray = webContent?.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                    
                     if websiteArray?.count > 1{
                         print(websiteArray)
                         let weatherArray = websiteArray![1].componentsSeparatedByString("</span>")
                         
                         if websiteArray!.count > 1{
-                            let weatherSammary = weatherArray[0]
+                            let weatherSammary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                            
+                            wasSuccessful = true
                             
                             dispatch_async(dispatch_get_main_queue(), {() -> Void in
-                                    self.resultLabel.text = weatherSammary
-                        })
+                                self.resultLabel.text = weatherSammary
+                            })
                             
                         }
                         
                     }
                     
-                
+                    
+                }
             }
+            task.resume()
+                if wasSuccessful == false{
+                    self.resultLabel.text = "Couldn't find the weather for that city - please try again."
+                }
+            
+        }else{
+                    self.resultLabel.text = "Couldn't find the weather for that city - please try again."
         }
         
-        task.resume()
+       
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+       
         
     }
 
