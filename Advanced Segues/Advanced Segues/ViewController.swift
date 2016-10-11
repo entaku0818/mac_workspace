cd6442f2f4919f82c7e3ba46068fdb1bd392ae3f
遠藤拓弥 <endo@MacBook-Pro-3.local>
2016-10-12 07:56:42 +0900

    思い出の場所を追加

---
+++ Advanced Segues/Advanced Segues/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Advanced Segues
+//
+//  Created by 遠藤拓弥 on 2016/10/07.
+//  Copyright © 2016年 遠藤拓弥. All rights reserved.
+//
+
+import UIKit
+
+var rowCounter:Int = 0
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print(rowCounter)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
